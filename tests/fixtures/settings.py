c02ef58b2ff83431d737b8f2020e07c8a917acb5
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2023-06-08 17:38:47 +0000

    docs: add pytest example to README

--- tests/fixtures/settings.py
+++ tests/fixtures/settings.py
@@ -2,10 +2,12 @@ from pysettings.base import BaseSettings
 from pysettings.options import Option
 from pysettings.validators import is_https_url
 
+
 # Class definition
 class TestSettings(BaseSettings):
     url = Option(validators=[is_https_url])
     description = Option()
+
 
 # Use settings in your application
 settings = TestSettings()
